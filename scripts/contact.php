1f1b9eed3bfe40f05fa0e530f78d516ec95f68f7
khaledhoues <khaledhoues@yahoo.fr>
2019-05-22 22:14:56 +0900

    fix email

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,7 +1,7 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	if(!empty($_POST['contact_name']) && !empty($_POST['contact_email']) && !empty($_POST['contact_message'])) {
-		$to = 'khaedhoues@yahoo.fr'; // Replace with your email.
+		$to = 'khaledhoues@yahoo.fr'; // Replace with your email.
 		$body = "Name: {$_POST['contact_name']}\n\nEmail: {$_POST['contact_email']}\n\nMessage: {$_POST['contact_message']}";
 		mail($to, "Contact Form Submission", $body, "From: {$_POST['contact_email']}");
 	}
